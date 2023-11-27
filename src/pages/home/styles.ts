a4b486a5fdbf1ee46c1f08d4df53087d62edeb65
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2023-11-27 16:05:57 +0000

    ClaimUsernameForm component

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -13,13 +13,14 @@ export const Hero = styled('div', {
   maxWidth: 480,
   padding: '0 $10',
 
-  [`${Heading}`]: {
+  // > Serve para setar que o estilo sera feita somente no primeiro nivel
+  [`> ${Heading}`]: {
     '@media(max-width: 600px)': {
       fontSize: '$6xl',
     },
   },
 
-  [`${Text}`]: {
+  [`> ${Text}`]: {
     maskType: '$2',
     color: '$gray200',
   },
