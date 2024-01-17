d91c8d6bd99d67cb6fae7311cb518a6bbfdaa70d
Ronaldo Silva <ronaldo.silva.jr0105@gmail.com>
2024-01-17 10:56:55 -0300

    Finalizing query for busy days

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -26,6 +26,7 @@ type CalendarWeeks = CalendarWeek[]
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 interface CalendarProps {
@@ -65,7 +66,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
     queryKey: [
       'blocked-dates',
       currentDate.get('year'),
-      currentDate.get('month'),
+      currentDate.get('month') + 1,
     ],
     queryFn: async () => {
       const response = await api.get(`/users/${username}/blocked-dates`, {
@@ -84,6 +85,9 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
     if (!blockedDates) {
       return []
     }
+
+    console.log('calendarWeeks ~ blockedDates', blockedDates)
+
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
@@ -123,10 +127,10 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
       ...daysInMonthArray.map((date) => {
         return {
           date,
-          disabled: !!(
+          disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day'))
-          ),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
